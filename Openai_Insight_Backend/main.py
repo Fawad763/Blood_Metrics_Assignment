b0c1b00c8c5e3fe2f0bf03b118058e5bfc5dc76d
Fawad763 <fawwadafzal26@gmail.com>
2024-11-09 01:01:34 +0500

    main.py

--- Openai_Insight_Backend/main.py
+++ Openai_Insight_Backend/main.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from models import PatientData
@@ -7,9 +8,9 @@ import config
 app = FastAPI()
 
 origins = [
-           "http://localhost:3000",
-           "https://blood-metrics-assignment.vercel.app"
-          ]
+    "http://localhost:3000",
+    "https://blood-metrics-assignment.vercel.app"  
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,4 +26,5 @@ async def get_insights(patient_data: PatientData):
         insights = openai_insight(patient_data.data)
         return {"insights": insights}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logging.error("Error in get_insights: %s", str(e))
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
